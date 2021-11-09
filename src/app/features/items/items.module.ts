85af323bfa92e2782e5a0368e0da07e2bf4ce627
woo <typkul@outlook.com>
2021-11-09 14:36:44 +0100

    consume lib

--- src/app/features/items/items.module.ts
+++ src/app/features/items/items.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ItemsComponent } from './components/items/items.component';
 import { RouterModule } from '@angular/router';
+import { MyLibModule } from 'my-lib';
 
 
 
@@ -13,7 +14,8 @@ import { RouterModule } from '@angular/router';
     CommonModule,
     RouterModule.forChild([
       { path: '', component: ItemsComponent}
-    ])
+    ]),
+    MyLibModule
   ]
 })
 export class ItemsModule { }
