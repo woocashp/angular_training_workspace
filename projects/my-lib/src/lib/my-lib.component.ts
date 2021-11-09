4c373011359fe849e7c9dbbb8cd598b6e275f0d9
woo <typkul@outlook.com>
2021-11-09 12:35:07 +0100

    my-lib works! (import my-lib)

--- projects/my-lib/src/lib/my-lib.component.ts
+++ projects/my-lib/src/lib/my-lib.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'lib-my-lib',
   template: `
     <p>
-      my-lib works!
+      my-lib works like a harm!
     </p>
   `,
   styles: [
