9957322bbc6882e0e29cc2b001a65366135816e9
woo <typkul@outlook.com>
2021-11-10 09:55:57 +0100

    cart with lib-grid

--- src/app/features/cart/components/cart/cart.component.ts
+++ src/app/features/cart/components/cart/cart.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CartStoreService } from '../../cart-store.service';
+import {  } from "module";
 
 @Component({
   selector: 'app-cart',
@@ -7,7 +10,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  cart$: Observable<any> = this.cartService.getState()
+
+  constructor(private cartService: CartStoreService) { }
 
   ngOnInit(): void {
   }
