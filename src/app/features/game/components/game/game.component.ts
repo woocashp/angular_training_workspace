930c4ad7538a00da251a9f808537fba3b9ea6fad
woo <typkul@outlook.com>
2021-11-10 11:50:31 +0100

    game via websocket (proxy)

--- src/app/features/game/components/game/game.component.ts
+++ src/app/features/game/components/game/game.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { EmbeddedTemplateAst } from '@angular/compiler';
+import { Component, EmbeddedViewRef, OnInit, TemplateRef, ViewChild, ViewContainerRef, ViewRef } from '@angular/core';
+import { catchError, fromEvent, map, of, retry, switchMap, throwError } from 'rxjs';
+import { Message } from 'src/app/shared/models/services.models';
 import { GameApiService } from 'src/app/shared/services/game.service';
 
 @Component({
@@ -8,10 +11,58 @@ import { GameApiService } from 'src/app/shared/services/game.service';
 })
 export class GameComponent implements OnInit {
 
-  constructor(private gameService: GameApiService) { }
+  @ViewChild('playerTpl') playerTpl!: TemplateRef<{ $implicit: Message}>
+
+  players: Map<string, EmbeddedViewRef<any>> = new Map();
+
+  constructor(
+    private gameService: GameApiService,
+    private container: ViewContainerRef
+  ) { }
 
   ngOnInit(): void {
-    this.gameService.getUser().subscribe(console.log)
+    this.gameService.getUser()
+    .pipe(
+      catchError((err: any) => {
+        this.register();
+        return throwError(() => err)
+      })
+    )
+    .subscribe(() => {
+      this.init();
+    });
   }
 
+  register() {
+    of('your name /^[a-zA-Z]{3,6}$/')
+      .pipe(
+        map(txt => prompt(txt)),
+        switchMap(username => this.gameService.register(username as string)),
+        catchError((error) => {
+          console.log('register error', JSON.stringify(error));
+          return throwError(() => error);
+        }),
+        retry(1)
+      )
+      .subscribe(this.init.bind(this))
+  }
+
+  init() {
+    this.gameService.messanger.subscribe((msg: Message) => {
+
+      const exists: EmbeddedViewRef<any> | undefined = this.players.get(msg.username || '')
+
+      if (exists) {
+        exists.context.$implicit = msg;
+      } else {
+        const view = this.container.createEmbeddedView(this.playerTpl, { $implicit: msg });
+        this.players.set(msg.username || '', view)
+      }
+    });
+
+    fromEvent<MouseEvent>(document.body, 'mousemove')
+      .subscribe(({ clientX, clientY }: MouseEvent) => {
+        this.gameService.messanger.next({ clientX, clientY });
+      })
+  }
 }
